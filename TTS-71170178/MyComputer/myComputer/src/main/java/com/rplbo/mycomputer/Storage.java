0b50d17f883747cfe74e116a6c3adbbb565f2951
putriade <54344789+putriade@users.noreply.github.com>
2022-10-24 10:05:13 +0700

    Add files via upload

--- TTS-71170178/MyComputer/myComputer/src/main/java/com/rplbo/mycomputer/Storage.java
+++ TTS-71170178/MyComputer/myComputer/src/main/java/com/rplbo/mycomputer/Storage.java
@@ -1,4 +1,13 @@
 package com.rplbo.mycomputer;
 
 public class Storage {
+    private String[] storage;
+
+    public int Storage(int storage){
+        return storage;
+    }
+
+    public int getData(int data){
+        return data;
+    }
 }
