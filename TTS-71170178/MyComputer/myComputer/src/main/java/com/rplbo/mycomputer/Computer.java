d88f76b27eedf4443976881e020c7b5ed1709303
putriade <54344789+putriade@users.noreply.github.com>
2022-10-24 10:01:15 +0700

    Add files via upload

---
+++ TTS-71170178/MyComputer/myComputer/src/main/java/com/rplbo/mycomputer/Computer.java
@@ -0,0 +1,25 @@
+package com.rplbo.mycomputer;
+
+public class Computer {
+    private int ID;
+    private int GlobalID;
+    private CableData cabledata;
+    private Storage storage;
+
+    public Storage getStorage(){
+        return storage;
+    }
+
+    public void detachCabledata(){
+       return;
+    }
+
+    public int getID(){
+
+    }
+
+    public void retrieveData(String ){
+
+    }
+
+}
---
+++ TTS-71170178/MyComputer/myComputer/src/main/java/com/rplbo/mycomputer/Main.java
@@ -0,0 +1,7 @@
+package com.rplbo.mycomputer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
